0cfee0d6907371701057d953ea97ddb521f5121d
Paul Awesome Kruger and Moises Zaragoza <paul_moises+miamiruby+mzararagoza@moiseszaragoza.com>
2013-04-29 19:38:00 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Matchmakersmatch::Application.config.secret_token = '2ea7dc630059fc921758e00e002584b0d24aac9a5f9576247261ff3f6217a43e1fff77e12697964572a04fc3cc816197ecc19c667bf62fa5cf85f1af9cd75300'
